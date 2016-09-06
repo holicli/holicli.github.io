d4343766e3bd4ba7087246af00ab2070802393ca
holicli <edwinvancleef@foxmail.com>
2016-09-06 22:14:46 +0800

    Update config.js

--- javascripts/config.js
+++ javascripts/config.js
@@ -1,11 +1,11 @@
 var disqus_shortname = 'holicli';			//Short Name of disqus, this parameter is for disqus comment box.
-var hostbase = 'http://holicliholicliholicliholicli.github.io';		//The absolute root address of your blog without '/' in the end.
-var githubname = 'holicliholicliholicli';						//Your username of GitHub.
-var repos = 'holicliholicli.github.io';					//Name of repository that contains your blog.
+var hostbase = 'http://holicli.github.io';		//The absolute root address of your blog without '/' in the end.
+var githubname = 'holicli';						//Your username of GitHub.
+var repos = 'holicli.github.io';					//Name of repository that contains your blog.
 var sitetitle = 'holicli';						//Name of your blog.
 var rss = '';									//Rss address of your blog.
-var suffix = '.md';								//Suffix of post files, '.' should be included.
-var branch = 'gh-pages';								//Branch of your blog repository. 'master' is default if you left it blank.
+var suffix = '';								//Suffix of post files, '.' should be included.
+var branch = '';								//Branch of your blog repository. 'master' is default if you left it blank.
 
 //Sample Config:
 //var disqus_shortname = 'sneezry';
